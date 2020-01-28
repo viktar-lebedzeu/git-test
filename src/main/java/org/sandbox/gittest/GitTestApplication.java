92b98ac8070306b6aae6569b01d54c2734ad3258
Viktar Lebedzeu <vlebedzeu@gmail.com>
2020-01-28 17:46:30 +0300

    Added GitTestApplication.java

---
+++ src/main/java/org/sandbox/gittest/GitTestApplication.java
@@ -0,0 +1,10 @@
+package org.sandbox.gittest;
+
+/**
+ *
+ */
+public class GitTestApplication {
+    public static void main(String... args) {
+        System.out.println("Running GitTestApplication...");
+    }
+}
